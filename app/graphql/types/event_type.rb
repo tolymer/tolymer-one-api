909005bf933d09bbee07ff0aef23ea3cc8d142c3
Kazuhito Hokamura <k.hokamura@gmail.com>
2020-06-08 23:51:33 +0900

    Allow null for tip

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -4,6 +4,6 @@ module Types
     field :event_date, GraphQL::Types::ISO8601Date, null: false
     field :participants, [Types::ParticipantType], null: false
     field :games, [Types::GameType], null: false
-    field :tip, Types::TipType, null: false
+    field :tip, Types::TipType, null: true
   end
 end
