1c610fa6a5f78d74394128c767c538e8ba19fb17
Kazuhito Hokamura <k.hokamura@gmail.com>
2022-05-06 21:02:02 +0900

    Add ping

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: ->(env) { [404, {}, []] }
+  get "/ping", to: ->(env) { [200, {}, ["pong"]] }
   post "/graphql", to: "graphql#execute"
   mount GraphqlPlayground::Rails::Engine, at: "/playground", graphql_path: "/graphql"
 end
