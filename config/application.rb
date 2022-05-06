6ca377ba83c6ec0b11dd64832e0efcb0392e05b4
Kazuhito Hokamura <k.hokamura@gmail.com>
2022-05-06 10:57:29 +0900

    Enebal playground

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,10 @@ module TolymerOne
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # For playground
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::ContentSecurityPolicy::Middleware
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: ->(env) { [404, {}, []] }
   post "/graphql", to: "graphql#execute"
   mount GraphqlPlayground::Rails::Engine, at: "/playground", graphql_path: "/graphql"
 end
