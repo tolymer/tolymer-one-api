919dec6d7307065cd439bccdd351c483b09b670d
Kazuhito Hokamura <k.hokamura@gmail.com>
2022-05-08 08:37:03 +0900

    Fix cors setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,11 +58,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins "*"
-      resource "/graphql", headers: :any, methods: [:post]
-    end
-  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,11 +75,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins "*"
-      resource "/graphql", headers: :any, methods: [:post]
-    end
-  end
 end
