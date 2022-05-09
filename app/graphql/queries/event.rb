09545224fb63a29834d6683ab9c07aaac2070474
Kazuhito Hokamura <k.hokamura@gmail.com>
2022-05-09 21:28:48 +0900

    Make event nullable

--- app/graphql/queries/event.rb
+++ app/graphql/queries/event.rb
@@ -1,9 +1,9 @@
 class Queries::Event < Queries::BaseQuery
-  type Types::EventType, null: false
+  type Types::EventType, null: true
 
   argument :token, String, required: true
 
   def resolve(token:)
-    ::Event.find_by!(token: token)
+    ::Event.find_by(token: token)
   end
 end
